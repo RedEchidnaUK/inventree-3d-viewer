9469755f06fa98e6cba90e939f2a0cc015f34073
Red Echidna UK <github@redechidna.com>
2025-04-19 13:10:16 +0100

    Updated model dispose

--- frontend/src/modelWrapper.tsx
+++ frontend/src/modelWrapper.tsx
@@ -2,42 +2,19 @@ import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { STLLoader } from "three/examples/jsm/loaders/STLLoader.js";
 import { useEffect } from "react";
+import { deepDispose } from "./dispose";
 
 interface ModelWrapperReturn {
   boundingSphere: any;
   modelJSX: JSX.Element;
 }
 
-
-function disposeModel(model: any) {
-  if (model && model.traverse) {
-    console.log("Disposing model with traverse");
-    model.traverse((obj: any) => {
-      if (obj.isMesh) {
-        obj.geometry.dispose();
-        if (obj.material.isMaterial) {
-          obj.material.dispose();
-          if (obj.material.map) {
-            obj.material.map.dispose();
-          }
-        }
-      }
-    });
-  }
-  else if (model) {
-    console.log("Disposing model");
-    model.dispose();
-  }
-}
-
-
 export function modelWrapper(url: string): ModelWrapperReturn {
   let myModel: any;
 
   useEffect(() => {
     return () => {
-      // Dispose previous model when component unmounts
-      disposeModel(myModel);
+      deepDispose(myModel);
     };
   }, [url]);
 
