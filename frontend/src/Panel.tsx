53747e74cdc6a1d5bb3bf42e1e8104888ab66897
Red Echidna UK <github@redechidna.com>
2025-04-19 12:54:03 +0100

    Fix css import

--- frontend/src/Panel.tsx
+++ frontend/src/Panel.tsx
@@ -18,9 +18,11 @@ import App from './App';
  */
 export function renderCAD3DViewerPanel(target: HTMLElement, context: any) {
     createRoot(target).render(
-        <MantineProvider theme={context.theme} defaultColorScheme={context.colorScheme}>
+        <>
             <link rel="stylesheet" href={`${context.host}static/plugins/${context.context.slug}/assets/style.css`} />
-            <App context={context} />
-        </MantineProvider>
+            <MantineProvider theme={context.theme} defaultColorScheme={context.colorScheme}>
+                <App context={context} />
+            </MantineProvider>
+        </>
     );
 }
